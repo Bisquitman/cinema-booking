831200587578dec642f0dd2165d6965f5d4494a9
Bisquitman <78824565+Bisquitman@users.noreply.github.com>
2021-10-10 22:25:26 +0300

    Исправил баг
    Исправил баг: стоимость билета добавлялась при клике на экран или свет от него

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let cost = 800;
 let totalPrice = 0;
 
 schemeSvg.addEventListener('click', (event) => {
-  if (!event.target.classList.contains('booked') && !event.target.classList.contains('scheme-bg') && !event.target.classList.contains('light')) {
+  if (!event.target.classList.contains('booked') && !event.target.classList.contains('scheme-bg') && !event.target.classList.contains('light') && !event.target.classList.contains('scheme-line')) {
     event.target.classList.toggle('active');
     const totalSeats = schemeSvg.querySelectorAll('.active').length
     totalPrice = totalSeats * cost;
